70bbd0f509e9c9bbaa288f4011cd80af697d9ddf
gustavoisidio <gisf@cin.ufpe.br>
2019-09-30 01:40:27 -0300

    resta triggers

--- 2.sql
+++ 2.sql
@@ -1,12 +1,14 @@
 CREATE TABLE employee (
-    emp_id INT PRIMARY,
+    emp_id INT,
     first_name VARCHAR(40),
     last_name VARCHAR(40),
     birth_date DATE,
     sex VARCHAR(1),
     salary INT,
     super_id INT, -- FK
-    branch_id INT -- FK
+    branch_id INT, -- FK
+    PRIMARY KEY ( emp_id )
+    
     -- FOREIGN KEY (super_id) REFERENCES
     -- FOREIGN KEY (branch_id) REFERENCES
 );
@@ -15,14 +17,14 @@ CREATE TABLE branch (
     branch_id INT PRIMARY KEY,
     branch_name VARCHAR(40),
     mgr_start_date DATE,
-    mgr_id INT -- FK
+    mgr_id INT, -- FK
     FOREIGN KEY (mgr_id) REFERENCES employee (emp_id) ON DELETE SET NULL
 );
 
 CREATE TABLE client (
     client_id INT PRIMARY KEY AUTO_INCREMENT,
     client_name VARCHAR(20),
-    branch_id INT -- FK
+    branch_id INT, -- FK
     FOREIGN KEY (branch_id) REFERENCES branch(branch_id)
     
 );
@@ -43,9 +45,11 @@ CREATE TABLE branch_supplier (
 );
 
 
-
-
 DESCRIBE employee;
+
+INSERT INTO employee VALUES(103, 'Angela', 'Martin', '1971-06-25', 'F', 63000, 102, 2);
+INSERT INTO employee VALUES(104, 'Kelly', 'Kapoor', '1980-02-05', 'F', 55000, 102, 2);
+INSERT INTO employee VALUES(105, 'Stanley', 'Hudson', '1958-02-19', 'M', 69000, 102, 2);
 
 
 
